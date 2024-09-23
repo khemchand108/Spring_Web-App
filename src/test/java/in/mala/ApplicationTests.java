eda3d98d9dd0f1f6d593274d900abdf56a57a30e
khemchand108 <kc@moodscale.in>
2024-09-23 21:50:04 +0530

    initial commit

---
+++ src/test/java/in/mala/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
